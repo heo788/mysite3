b3d2db8e3107f2a345ff740f394f624872ba7bc7
heo788 <air267777@gmail.com>
2025-05-08 20:00:09 +0900

    .

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import HomePage from './pages/home/HomePage';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Header from './components/common/Header';
+import Footer from './components/common/Footer';
+import AboutMePage from './pages/aboutMe/AboutMePage';
 
-function App() {
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <BrowserRouter basename="/mysite3">
+        <div className="App">
+          <header className="app-header">
+            <Header />
+          </header>
+          <main>
+            <Routes>
+              <Route path="/" element={<Navigate to="/home" />} />
+              <Route path="/home" element={<HomePage />} />
+              <Route path="/about-me" element={<AboutMePage />} />
+              {/* <Route path="/gallery" element={<GalleryPage />} />
+              <Route path="/blog" element={<BlogPage />} />
+              <Route path="/my-story" element={<MyStoryPage />} /> */}
+            </Routes>
+            <footer className="app-footer">
+              <Footer />
+            </footer>
+          </main>
+        </div>
+      </BrowserRouter>
   );
 }
 
